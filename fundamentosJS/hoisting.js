d5099591bf33d07826bcad5c0a612e78bd7b12ee
JonasMoura-J <jonasmourat9090@gmail.com>
2020-10-04 10:54:29 -0300

    atualizando o hoisting

--- fundamentosJS/hoisting.js
+++ fundamentosJS/hoisting.js
@@ -3,3 +3,10 @@
  var a = 2 
 
  console.log('a =',a)
+
+
+ console.log('a =',b)  //Com let, não acontece o hoisting, nesse caso dará erro porque no primeiro console, a variavel não foi inicializada
+
+ let b = 2 
+
+ console.log('a =',b)
